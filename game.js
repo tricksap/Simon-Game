26c8200ed28bd95fe65a0cd3fbf4ba63a10ecef2
Patrick <tricksap@gmail.com>
2021-02-16 10:09:38 +0800

    first Commit (generate random pattern)

---
+++ game.js
@@ -0,0 +1,25 @@
+var  buttonColors = [ "red", "blue","green","yellow"]
+var gamePattern = []
+var userClickedPattern= []
+
+$(".btn").click(function () { 
+    var userChosenColour= $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour  )
+ });
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*4)
+    var randomChosenColour = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColour)
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour)
+}
+
+function playSound(color){
+    var audio = new Audio("sounds/" + color + ".mp3");
+    audio.play();
+
+}
+
+   
