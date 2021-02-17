cfa8c397a29e92e3f3b0247a42e50401d4e674ba
Patrick <tricksap@gmail.com>
2021-02-17 12:02:37 +0800

    add next level and answer checker

--- game.js
+++ game.js
@@ -1,16 +1,53 @@
 var  buttonColors = [ "red", "blue","green","yellow"]
-var gamePattern = []
-var userClickedPattern= []
+var gamePattern = [];
+var userClickedPattern= [];
+var gameStart= false;
+var level = 0;  
+
+
+
+function checkAnswer(currentLevel){
+    var i = 0
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel])
+    {
+        console.log("success")
+    
+        if(userClickedPattern.length  === gamePattern.length){
+            setTimeout(function () {
+                nextSequence();
+              }, 1000);
+        }
+    }
+    else{
+        console.log("wrong")
+    }
+}
+
+
+$(document).keypress(function(){
+    if (!gameStart){
+        gameStart = true;
+        nextSequence(); 
+    }
+});
+
 
 $(".btn").click(function () { 
-    var userChosenColour= $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour)
+    var userChosenColor= $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor)
 
-    animatePress(userChosenColour)
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length-1)
  });
  
+
 function nextSequence(){
+    userClickedPattern = [];
+    
+    level = level + 1;
+    $("#level-title").text("Level "+ level);
+
     var randomNumber = Math.floor(Math.random()*4)
     var randomChosenColour = buttonColors[randomNumber];
     gamePattern.push(randomChosenColour)
